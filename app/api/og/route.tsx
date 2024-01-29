74cfc4f478d907717087fb68a522bea3e6d4e686
michellebakels <michelle.bakels@gmail.com>
2024-01-29 17:51:32 -0500

    add og image

---
+++ app/api/og/route.tsx
@@ -0,0 +1,29 @@
+import { ImageResponse } from 'next/og';
+import image from '../../../public/og.png'
+// App router includes @vercel/og.
+// No need to install it.
+ 
+export const runtime = 'edge';
+ 
+export async function GET() {
+  const background = "https://basehub.earth/cdn-cgi/imagedelivery/AcMAerS13uRNdcqmxOA-IQ/273ebe35-9545-4d3b-f5c4-120b5b380d00/public"
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url(${background})`
+        }}
+      />
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
