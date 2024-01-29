f3b719f3598e45c176d434780f8be9786e8de1a3
michellebakels <michelle.bakels@gmail.com>
2024-01-29 18:55:33 -0500

    og

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,27 +5,21 @@ import { ImageResponse } from 'next/og';
 export const runtime = 'edge';
  
 export async function GET() {
-  const background = "https://basehub.earth/cdn-cgi/imagedelivery/AcMAerS13uRNdcqmxOA-IQ/273ebe35-9545-4d3b-f5c4-120b5b380d00/public"
-  
-  const imageData = await fetch(new URL('./image.png', import.meta.url)).then(
-    (res) => res.arrayBuffer(),
-  );
+  const image = 
+  "https://basehub.earth/cdn-cgi/imagedelivery/AcMAerS13uRNdcqmxOA-IQ/273ebe35-9545-4d3b-f5c4-120b5b380d00/public"
   
   return new ImageResponse(
     (
       <div
         style={{
-          background: 'white',
+          backgroundImage: `url(${image})`,
           width: '100%',
           height: '100%',
           textAlign: 'center',
           justifyContent: 'center',
           alignItems: 'center',
-          backgroundImage: `url(${background})`
         }}
-      >
-        <img width="256" height="256" src={imageData} />
-      </div>
+      />
     ),
     {
       width: 1200,
