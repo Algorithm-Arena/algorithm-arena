d57548b66d82ed2391def883d52abd453ca02772
michellebakels <michelle.bakels@gmail.com>
2024-01-29 18:26:01 -0500

    og

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -5,7 +5,7 @@ import image from '../../../public/og.png'
  
 export const runtime = 'edge';
  
-export async function GET() {
+export default async function GET() {
   const background = "https://basehub.earth/cdn-cgi/imagedelivery/AcMAerS13uRNdcqmxOA-IQ/273ebe35-9545-4d3b-f5c4-120b5b380d00/public"
   return new ImageResponse(
     (
